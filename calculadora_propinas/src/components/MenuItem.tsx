df76a1e38dc76ba1c83c9de6eeca54dd95622c91
Maury <lynuxazura@gmail.com>
2025-03-11 21:25:10 -0600

    MenuItems Functions 1

--- calculadora_propinas/src/components/MenuItem.tsx
+++ calculadora_propinas/src/components/MenuItem.tsx
@@ -1,13 +1,15 @@
-import { MenuItem } from "../types"
+import type { MenuItem } from "../types"
 
 type MenuItemProps = {
     item: MenuItem
+    addItem:(item:MenuItem)=>void
 } 
 
-export default function MenuItem({item}:MenuItemProps) {
+export default function MenuItem({item,addItem}:MenuItemProps) {
   return (
     <button
     className="border-2 border-black w-full p-3 flex justify-between hover:bg-green-200"
+    onClick={()=>addItem(item)}
     >
         <p>{item.name}</p>
         <p className="font-black">${item.price}</p>
--- calculadora_propinas/src/types/index.ts
+++ calculadora_propinas/src/types/index.ts
@@ -1,5 +1,10 @@
+//Items a mostrar
 export type MenuItem = {
     id:number
     name: string
     price: number
+}
+
+export type OrderItem = MenuItem & {
+    quantity:number
 }
